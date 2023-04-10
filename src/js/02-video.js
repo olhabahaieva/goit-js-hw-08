7f5fb4e56be6c8d0af6db371e67c03a61f7ee7bb
Olha Bahaieva <bahaieva.olha@gmail.com>
2023-04-10 20:58:57 +0300

    2nd task is done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,16 +1,28 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 console.log(Player);
 
-
+const STORAGE_KEY = "videoplayer-current-time";
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on('play', function() {
-    console.log('played the video!');
-});
+player.on('timeupdate', throttle (function(data) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(data.seconds))
+}), 1000);
 
-player.getVideoTitle().then(function(title) {
-    console.log('title:', title);
+player.setCurrentTime(30.456).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+    localStorage.getItem(STORAGE_KEY);
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
 });
