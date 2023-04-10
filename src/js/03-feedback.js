d745334168c1a8038d208795aceb9215551f9716
Olha Bahaieva <bahaieva.olha@gmail.com>
2023-04-10 22:46:10 +0300

    3rd task is done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,21 +1,38 @@
 import throttle from 'lodash.throttle';
 
 //element to get event listener from
-const inputField = document.querySelector('form');
-
-//Event listener
-inputField.addEventListener('submit', onsubmit);
+const form = document.querySelector('.feedback-form');
 
 //Local storage key
 const STORAGE_KEY = "feedback-form-state";
 
-//Function
-function onsubmit(evt){
-    evt.preventDefault();
-    const inputElement = inputField.querySelector('input[type="text]')
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(inputElement));
-    const inputResult = localStorage.getItem(STORAGE_KEY);
-    const parsedResult = JSON.parse(inputResult);
-    console.log(parsedResult);
-    
+// Load saved data from storage (if any)
+const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+if (savedData) {
+  form.email.value = savedData.email || '';
+  form.message.value = savedData.message || '';
 }
+
+// Function to save form data to localStorage
+const saveDataToLocalStorage = throttle(() => {
+  const data = {
+    email: form.email.value,
+    message: form.message.value
+  };
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
+}, 500);
+
+// Event listener on form submit
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const data = {
+    email: form.email.value,
+    message: form.message.value
+  };
+  console.log(data);
+  localStorage.removeItem(STORAGE_KEY);
+  form.reset();
+});
+
+// Event listener on form input
+form.addEventListener('input', saveDataToLocalStorage);
